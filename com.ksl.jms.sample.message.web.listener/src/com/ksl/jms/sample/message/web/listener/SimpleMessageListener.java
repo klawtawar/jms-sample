a2c1973e5f38b18a67938e804a6142d6e9208f77
kunal <klawtawar@gmail.com>
2018-12-25 11:25:21 -0600

    updates

---
+++ com.ksl.jms.sample.message.web.listener/src/com/ksl/jms/sample/message/web/listener/SimpleMessageListener.java
@@ -0,0 +1,19 @@
+package com.ksl.jms.sample.message.web.listener;
+
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+ 
+public class SimpleMessageListener implements MessageListener {
+	@Override
+	public void onMessage(Message message) {
+		try {
+			TextMessage msg = (TextMessage) message;
+			System.out.println(msg.getText());
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+}
